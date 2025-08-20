07e3cc6dd9a000713f5f5b4b370c917a8d441fc8
michaelWebDeveloper <mikle.web.developer@gmail.com>
2025-08-20 13:03:54 +0300

    Познакомились с JS.

---
+++ 20.08.2025/main.js
@@ -0,0 +1,66 @@
+// console.log('Hello world!')
+// console.log(typeof true)
+//
+// // Арифметические операторы
+// console.log(2+2)
+// console.log(2*2)
+// console.log(4/2)
+// console.log(4-2)
+// console.log(5%2)
+// console.log(2**3)
+//
+// // Операторы сравнения
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<2)
+// console.log(2<=2)
+// console.log(2=='2')
+// console.log(2==='2')
+// console.log(2===2)
+// console.log(2!=2)
+// console.log(2!=='2')
+//
+// // Логические операторы
+// console.log(1||false)
+// console.log(1&&false)
+// console.log(!false)
+//
+// // Операторы присваивания
+// let i = 1
+// i++
+// console.log(i)
+// i--
+// console.log(i)
+// i+=10
+// // i = i + 10
+// console.log(i)
+
+// let num = 10
+// let exp = 3
+// let result = num ** exp
+// console.log(result)
+//
+// let i = 0
+// while(i<10){
+//     i++
+//     console.log(i)
+// }
+//
+// for(let j = 0; j<10; j++){
+//     console.log(j)
+// }
+
+// for(let j = 0; j<100; j++){
+//     if(j%2===0){
+//         console.log(j)
+//     }
+// }
+
+let x = 0
+while(x<10){
+    if(x%5===0){
+        console.log(x)
+    }
+    x++
+
+}
