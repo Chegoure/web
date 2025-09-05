bac64375dfb4fda3df24872c505ecf645850ce8f
Chegoure <timildmitri4@icloud.com>
2025-09-05 12:57:17 +0300

    Практика с JS.

---
+++ 05.09.2025/main.js
@@ -0,0 +1,46 @@
+/*
+Сделаем практическое задание которое называется fizz buzz.
+Цель: Сделать цикл от 1 до 100 и вывести:
+- fizz если число делится без остатка на 3.
+- buzz если число делится без остатка на 5.
+- fizzbuzz если число делится без остатка на 3 и на 5.
+*/
+
+
+// for(let i = 1; i<=100; i++){
+//     if(i%3 === 0 && i%5 === 0){
+//         console.log(i + ' fizzbuzz')
+//     } else if(i%3 === 0) {
+//         console.log(i + ' fizz')
+//     } else if(i%5 === 0){
+//         console.log(i + ' buzz')
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+
+// for(let i = 1; i<=100; i++){
+//     let res = ''s
+//
+//     if(i%3 === 0){
+//         res =  res + 'fizz'
+//     }
+//
+//     if(i%5 === 0){
+//         res = res + 'buzz'
+//     }
+//
+//     console.log(i + ' ' + res)
+// }
+
+
+// let res = 1
+// let exp = 5
+// let num = 4
+//
+// for(let i = 0; i<exp; i++){
+//     res = res * num
+//     console.log(res)
+// }
+// console.log(6**10)
