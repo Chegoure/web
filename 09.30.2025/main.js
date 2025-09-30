8b5646bd57ec75337f0b5e92bf0357b5c3e5e345
Chegoure <timildmitri4@icloud.com>
2025-09-30 22:29:39 +0300

    Теоретическое занятие по JS. Знакомство с BOM \ DOM.

---
+++ 09.30.2025/main.js
@@ -0,0 +1,22 @@
+// const Vasya = {
+//     name: 'Васян',
+//     favFood: ['Борщ', 'Чахартма', 'Плов'],
+//     greetings(){
+//         let favFoodString = ''
+//         this.favFood.forEach((foodItem, index) => {
+//             favFoodString += index === this.favFood.length-1 ? foodItem + '.' : foodItem + ', '
+//         })
+//         console.log(`Здарова, я ${this.name} ! Я обожаю похавать ${favFoodString} `)
+//     }
+// }
+// Vasya.greetings()
+
+const parentElemnt = document.getElementById('app')
+
+const addBox = () => {
+    const newEl = document.createElement('div')
+    newEl.innerHTML = 'hello world'
+    newEl.classList.add('box')
+    parentElemnt.append(newEl)
+}
+addBox()
