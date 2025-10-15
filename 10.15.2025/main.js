e3a61ed992c210674b750427300b973e71f5fa46
Chegoure <timildmitri4@icloud.com>
2025-10-15 12:37:51 +0300

    Практическое занятие по JS. Работа с HTML элементами и Обработчиками событий.

---
+++ 10.15.2025/main.js
@@ -0,0 +1,41 @@
+// const appEl = document.getElementById('app')
+// const buttonEl = document.getElementById('btn')
+// buttonEl.addEventListener('click', ()=>{
+//     const redBlockEl = document.createElement('div')
+//     redBlockEl.style.backgroundColor = 'red'
+//     redBlockEl.style.width = '300px'
+//     redBlockEl.style.height = '300px'
+//     appEl.append(redBlockEl)
+// })
+
+// let isDrawing = false
+// let colorString = ''
+// const inputEl = document.getElementById('color')
+//
+// document.addEventListener('mousemove', (e)=>{
+//     if(!isDrawing) return
+//     console.log(e)
+//     const redBlockEl = document.createElement('div')
+//     redBlockEl.style.backgroundColor = colorString
+//     redBlockEl.style.position = 'absolute'
+//     redBlockEl.style.top = (e.clientY + 5) + 'px'
+//     redBlockEl.style.left = (e.clientX + 5) + 'px'
+//     redBlockEl.style.width = '3px'
+//     redBlockEl.style.height = '3px'
+//     document.body.append(redBlockEl)
+// })
+//
+// inputEl.addEventListener('input', (e)=>{
+//     console.log(e)
+//     console.log(e.target)
+//     colorString = e.target.value
+// })
+//
+// document.addEventListener('mousedown', ()=>{
+//     isDrawing = true
+// })
+// document.addEventListener('mouseup', ()=>{
+//     isDrawing = false
+// })
+
+
