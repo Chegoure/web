1f0aa4de1925edff3cbe04f05ba429d048e0cc00
Chegoure <timildmitri4@icloud.com>
2025-11-26 12:01:20 +0300

    Практическое занятие по JS. Проект burgers. Создание системы управления кол-вом продуктов в заказе.

--- 11.08.2025/burgers/src/Order.js
+++ 11.08.2025/burgers/src/Order.js
@@ -13,16 +13,35 @@ export default {
         console.log(this.products)
         this.renderProducts()
     },
-    removeProduct(burgerId){
-
+    removeProduct(burgerRemoveId){
+        const productToRemoveIndex = this.products.findIndex(product => product.burgerId === burgerRemoveId)
+        if(this.products[productToRemoveIndex].quantity > 1){
+            this.products[productToRemoveIndex].quantity--
+        }else{
+            this.products.splice(productToRemoveIndex,1)
+        }
+        this.renderProducts()
     },
     renderProducts(){
         orderEl.innerHTML = ''
         this.products.forEach((product)=>{
             const orderItemEl = renderElement('div', orderEl, {class: 'order-item'})
             renderElement('span', orderItemEl, {content: product.burger.name, class: 'burger-name'})
-            renderElement('span', orderItemEl, {content: 'x ' + product.quantity})
+
+            const orderQuantityEl = renderElement('div', orderItemEl, {class: 'order-quantity'})
+            const arrowRemoveProductEl = renderElement('span', orderQuantityEl, {content: '<', class: 'arrow-remove-product'})
+            renderElement('span', orderQuantityEl, {content: ' x ' + product.quantity})
+            const arrowAddProductEl = renderElement('span', orderQuantityEl, {content: ' >', class: 'arrow-add-product'})
+
             renderElement('span', orderItemEl, {content: product.burger.price * product.quantity})
+
+            arrowAddProductEl.addEventListener('click', ()=>{
+                this.addProduct(product.burger)
+            })
+            arrowRemoveProductEl.addEventListener('click', ()=>{
+                this.removeProduct(product.burgerId)
+            })
+
         })
     }
 }
