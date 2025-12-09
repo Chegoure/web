8278641d9dfaa267a46ac54ebbbd4287eb8ba458
Chegoure <timildmitri4@icloud.com>
2025-12-09 13:38:20 +0300

    Практическое занятие по JS.Работа над проектом burgers закончена в формате нативного, модульного JS. Добавлены активные заказы в список заказов.
    Инициализация проекта Vue.js. Знакомство с фреймворком.

---
+++ 12.09.2025/vite-project/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import {ref} from "vue";
+  import myFirstComponent from "./components/myFirstComponent.vue"
+
+  const count = ref(1)
+</script>
+
+<template>
+  <div>
+    <h1 @click="count++">hi world {{count}}</h1>
+    <myFirstComponent></myFirstComponent>
+  </div>
+</template>
+
