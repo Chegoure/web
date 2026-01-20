660dab05cb0aa8edeb7c3be8440bd3c76a6e90ce
Chegoure <timildmitri4@icloud.com>
2026-01-20 11:57:47 +0300

    Практическое занятие по Vue. Практика построения интерфейса списка.

---
+++ 20 - 20.01.2026/vite-project/src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+import {ref} from "vue";
+const task = ref('')
+const list = ref([])
+
+const add = () => {
+  list.value.push({
+    taskName: task.value,
+    isDone: false
+  })
+}
+
+const remove = (index) => {
+  list.value.splice(index, 1)
+}
+
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="inner">
+    <input v-model="task" type="text">
+    <button @click="add">+</button>
+    </div>
+    <ul>
+      <li v-for="(item, index) in list">
+        {{item.taskName}}
+        <input v-model="item.isDone" type="checkbox">
+        <span @click="remove(index)">x</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+.wrapper {
+  width: 500px;
+}
+
+.inner {
+  padding-right: 16px;
+  display: flex;
+}
+
+input {
+  width: 100%;
+}
+
+ul {
+  margin: 0;
+  padding: 0;
+}
+
+li {
+
+  padding-top: 10px;
+  list-style: none;
+  display: grid;
+  grid-template-columns: 450px 1fr 1fr;
+}
+
+</style>
---
+++ 20 - 20.01.2026/vite-project/src/components/columns.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {ref} from "vue";
+
+const list1 = ref(['Jim', 'Randy'])
+const list2 = ref(['Julian', 'Ricky'])
+
+const list1Move = (name, index) => {
+  console.log(name, index)
+  list1.value.splice(index, 1)
+  list2.value.push(name)
+}
+
+const list2Move = (name, index) => {
+  console.log(name, index)
+  list2.value.splice(index, 1)
+  list1.value.push(name)
+}
+
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="(name, index) in list1" @click="list1Move(name, index)">{{ name }}</li>
+    </ul>
+    <ul>
+      <li v-for="(name, index) in list2" @click="list2Move(name, index)">{{ name }}</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+div {
+  display: flex;
+  gap: 20px;
+}
+
+</style>
