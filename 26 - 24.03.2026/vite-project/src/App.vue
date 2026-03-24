c04eb9478ec7f8d0cf95dc9c65fa2793f5aa8e00
Chegoure <timildmitri4@icloud.com>
2026-03-24 12:46:03 +0300

    Практическое занятие по Vue. Продолжение разработки учебного форума, основанного на express сервере. Реализованны все функции сервера

---
+++ 26 - 24.03.2026/vite-project/src/App.vue
@@ -0,0 +1,86 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import Register from './components/Register.vue'
+import Login from './components/Login.vue'
+import Forum from './components/Forum.vue'
+
+const user = ref({})
+const isLogin = ref(true)
+
+const signIn = (u) => {
+  user.value = u
+  localStorage.setItem('user', JSON.stringify(u))
+}
+
+onMounted(()=>{
+  const savedUser = localStorage.getItem('user')
+  if (savedUser) {
+    user.value = JSON.parse(savedUser)
+  }
+})
+
+</script>
+
+<template>
+  <div class="container-big">
+    <div v-if="user.id">
+      <Forum :user="user" />
+    </div>
+    <div v-else>
+      <Login v-if="isLogin" @login="signIn" />
+      <Register v-else @register="signIn" />
+
+      <button @click="isLogin = !isLogin">
+        {{ isLogin ? 'Зарегистрироваться' : 'Войти' }}
+      </button>
+    </div>
+  </div>
+</template>
+
+<style>
+* {
+  font-family: 'Press Start 2P', sans-serif;
+  /* font-weight: 400; */
+  /* font-style: normal; */
+}
+
+body {
+  background-color: #f4e9d3;
+}
+
+li {
+  list-style-type: none;
+  cursor: pointer;
+}
+
+.container-big {
+  width: 1400px;
+  height: 40vw; /* Пока что */
+  margin: 30px auto;
+  position: relative; /*Добавил для блюра*/
+  z-index: 0; /*Добавил для блюра*/
+
+  border: 5px solid black;
+  border-radius: 8px;
+
+  background-image: url('./assets/bg.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
+}
+
+/* .container-big::before {
+  content: "";
+  position: absolute;
+  inset: 0;
+
+  background-image: url('./assets/bg.png');
+  background-size: cover;
+  background-position: center;
+
+  filter: blur(10px);
+
+  z-index: -1;
+} */
+</style>
