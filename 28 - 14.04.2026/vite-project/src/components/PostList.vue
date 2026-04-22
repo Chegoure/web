a61c408e8ce48d80093336cb4231f10b77690da9
Chegoure <timildmitri4@icloud.com>
2026-04-22 10:22:41 +0300

    Стилизация панели пагинации.

--- 28 - 14.04.2026/vite-project/src/components/PostList.vue
+++ 28 - 14.04.2026/vite-project/src/components/PostList.vue
@@ -34,31 +34,57 @@ const deletePost = (id) => {
         {{ post.author_name }}:
         {{ post.content }}
       </div>
-      <span @click="deletePost(post.id)">x</span>
+      <span @click="deletePost(post.id)" class="post-delete">x</span>
     </li>
   </ul>
   <div v-if="posts.length" class="pagination-panel">
-    <span v-if="page > 0" @click="setPage(page - 1)"><</span>
     <div>
-      <span>{{page}}</span>
-      <span v-if="lastPage > page + 1" @click="setPage(page + 1)" >{{page + 1}}</span>
-      <span v-if="lastPage > page + 1" @click="setPage(page + 2)" >{{page + 2}}</span>
-      <span v-if="lastPage > page + 1" @click="setPage(page + 1)" >></span>
-      <span @click="setPage(lastPage)"> ...{{lastPage}}</span>
+      <span v-if="page > 0" @click="setPage(page - 1)" class="panel-switch-page"><</span>
+      <span class="panel-switch-page">{{page}}</span>
+      <span v-if="lastPage > page + 1" @click="setPage(page + 1)" class="panel-switch-page">{{page + 1}}</span>
+      <span v-if="lastPage > page + 1" @click="setPage(page + 2)" class="panel-switch-page">{{page + 2}}</span>
+      <span v-if="lastPage > page + 1" @click="setPage(page + 1)" class="panel-switch-page">></span>
+      <span @click="setPage(lastPage)" class="panel-switch-page"> ...{{lastPage}}</span>
     </div>
   </div>
 </template>
 
 <style scoped>
+
 .post {
+  display: flex;
+
   padding: 10px;
   margin-bottom: 14px;
 
-  /* background-color: #f4e9d3; */
   background-color: #efe9db;
-
   border: 5px solid black;
   border-radius: 8px;
   box-shadow: 4px 4px 0 0 rgba(0, 0, 0, 0.9);
+
+  min-height: 126px;
+}
+
+.post-delete {
+  font-weight: bold;
+  margin-left: 5px;
+}
+
+.pagination-panel {
+  display: flex;
+  justify-content: center;
+  padding-top: 12px;
+}
+
+.panel-switch-page {
+  cursor: pointer;
+  padding: 5px;
+  margin: 0 5px;
+  background-color: #efe9db;
+  border: 5px solid black;
+  border-radius: 8px;
+  box-shadow:
+   4px 4px 0 0 rgba(0, 0, 0, 0.9);
+
 }
 </style>
